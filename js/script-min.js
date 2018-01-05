c90abf58c1a58e1d8f836f9e41efec420283fb4c
Tim Christiansen <tim-netz@web.de>
2018-01-05 22:42:09 +0100

    dumb changes

---
+++ js/script-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".options").click(function(){$(this).closest(".container").hasClass("landing")?$(".settingspage").removeClass("hide"):$(".settingspage").addClass("hide")})});
--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
- 
+  $('.options').click(function() {
+    if ( $(this).closest(".container").hasClass("landing") ){
+      $('.settingspage').removeClass('hide');
+    } else {
+      $('.settingspage').addClass('hide');
+    }
+
+
+  });
 });
