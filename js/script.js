e7a4d289f734adae33928477f1a7cb089eddd76d
Hauke Grothues <hauke.grothues@web.de>
2018-01-09 20:18:39 +0100

    Add animation and default color

--- js/script.js
+++ js/script.js
@@ -17,25 +17,29 @@ $(document).ready(function(){
       case 'total g cpicker':
         $("body").removeClass().addClass("g");
         c.circleProgress({
-          fill: {color: "#2ECC71"}
+          fill: {color: "#2ECC71"},
+          animation: false
         });
         break;
       case 'total o cpicker':
         $("body").removeClass().addClass("o");
         c.circleProgress({
-          fill: {color: "#E67E22"}
+          fill: {color: "#E67E22"},
+          animation: false
         });
         break;
       case 'total b cpicker':
         $("body").removeClass().addClass("b");
         c.circleProgress({
-          fill: {color: "#3498DB"}
+          fill: {color: "#3498DB"},
+          animation: false
         });
         break;
       case 'total r cpicker':
         $("body").removeClass().addClass("r");
         c.circleProgress({
-          fill: {color: "#E74C3C"}
+          fill: {color: "#E74C3C"},
+          animation: false
         });
         break;
     }
