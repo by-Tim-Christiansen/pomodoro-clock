05df41c2e846872be580baef4b787818c3b66416
Hauke Grothues <hauke.grothues@web.de>
2018-01-11 19:02:02 +0100

    shorten the code

--- js/script.js
+++ js/script.js
@@ -50,38 +50,30 @@ $(".num").click(function() {
 });
 
   // change color theme
-    $(".cpicker").click(function() {
-      var className = $(this).attr('class');
-      switch(className){
+  function changeTheme(colorClass, hex) {
+    $("body").removeClass().addClass(colorClass);
+    c.circleProgress({
+      fill: {color: hex},
+      animation: false
+    });
+  }
+
+  $(".cpicker").click(function() {
+    var className = $(this).attr('class');
+    switch(className){
       case 'total g cpicker':
-        $("body").removeClass().addClass("g");
-        c.circleProgress({
-          fill: {color: "#2ECC71"},
-          animation: false
-        });
+        changeTheme("g", "#2ECC71");
         break;
       case 'total o cpicker':
-        $("body").removeClass().addClass("o");
-        c.circleProgress({
-          fill: {color: "#E67E22"},
-          animation: false
-        });
+        changeTheme("o", "#E67E22");
         break;
       case 'total b cpicker':
-        $("body").removeClass().addClass("b");
-        c.circleProgress({
-          fill: {color: "#3498DB"},
-          animation: false
-        });
+        changeTheme("b", "#3498DB");
         break;
       case 'total r cpicker':
-        $("body").removeClass().addClass("r");
-        c.circleProgress({
-          fill: {color: "#E74C3C"},
-          animation: false
-        });
+        changeTheme("r", "#E74C3C");
         break;
-    }
+      }
     });
 
   // toggle between timer and settings page
