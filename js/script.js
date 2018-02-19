0b7b3bc50283bd3583b5ae9f1ed3f5a09100ddc3
Hauke Grothues <hauke.grothues@gmail.com>
2018-02-19 15:14:11 +0100

    Restore session and break values

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ c.circleProgress({
 $(document).ready(function(){
 
   // declare variables for timer
-  var sessionLength = 0.1, breakLength = 0.05;
+  var sessionLength = 25, breakLength = 5;
   var pomoTimer = new Timer();
   var totalSessions = 0;
   var currentColor = "#E74C3C";
