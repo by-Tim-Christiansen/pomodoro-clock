84a4de70f9fb066661d850763aef7f95b459c85b
Hauke Grothues <hauke.grothues@gmail.com>
2018-09-22 17:58:47 +0200

    remove data from php

--- php/contact.php
+++ php/contact.php
@@ -18,8 +18,8 @@ try {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'flellow99@gmail.com';
-    $mail->Password = 'yHRM+f>4CM=D?aw#';
+    $mail->Username = '';
+    $mail->Password = '';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
