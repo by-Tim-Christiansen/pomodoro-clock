a60bd49763c95a6efa5da08ac3e947148f5b852d
Hauke Grothues <hauke.grothues@gmail.com>
2018-09-23 18:10:29 +0200

    fix

--- php/contact.php
+++ php/contact.php
@@ -18,8 +18,8 @@ try {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'hauke.grothues@gmail.com';
-    $mail->Password = 'Pemonboys98';
+    $mail->Username = '';
+    $mail->Password = '';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
